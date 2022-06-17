5791cb94e72b242348f2414715e0204828b114b4
KRISTINAKIM <kristinabridikyte@gmail.com>
2022-06-17 14:31:00 +0300

    Add style

---
+++ app.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {stickyScroll()};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickyScroll() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
